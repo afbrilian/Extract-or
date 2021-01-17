b98586faa9298345fb6b2e0f47990bc1d4586a2f
afbrilian <af.brilian@gmail.com>
2021-01-17 08:28:27 +0700

    feat: change strorage approach

--- web-app/src/lib/storage.js
+++ web-app/src/lib/storage.js
@@ -2,29 +2,13 @@ import { firebaseStorage } from './provider/firebase'
 
 const upload = async (user, files) => {
   const storageRef = firebaseStorage.ref()
+  const path = `${user.uid}/pdf`
   const promises = []
   for (const file of files) {
-    promises.push(uploadImageAsPromise(user, storageRef, file))
+    const userRef = storageRef.child(`${path}/${file.filename}`) 
+    promises.push(userRef.put(file))
   }
   await promises
-}
-
-const uploadImageAsPromise = (user, storageRef, file) => {
-  return new Promise((resolve, reject) => {
-    const userRef = storageRef.child(`${user.uid}/pdf/${file.filename}`)
-    const task = userRef.put(file)
-
-    task.on(
-      'state_changed',
-      function progress() {},
-      function error(err) {
-        reject(err)
-      },
-      function complete() {
-        resolve()
-      }
-    )
-  })
 }
 
 export { upload }
